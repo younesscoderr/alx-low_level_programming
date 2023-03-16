b738173209e0c9b8e47fba9517b4fe9ea48ef20d
youness <younesselayyoubi@hotmail.com>
2023-03-16 10:14:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main-Entry point
+ * Description:Prints a string to standard output
+ * Return:Alwayse 0 (Succuss)
+ */
 int main(void)
-
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
