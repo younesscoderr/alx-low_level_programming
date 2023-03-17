3fe3da2b8da021ceb9492f0e1bd9b15d6f4c5b6c
youness <younesselayyoubi@hotmail.com>
2023-03-17 08:28:37 -0700

    prints the alphabet in lowercase except 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase exept 'q' and 'e'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+{
+putchar(letter);
+}
+}
+putchar('\n');
+return (0);
+}
