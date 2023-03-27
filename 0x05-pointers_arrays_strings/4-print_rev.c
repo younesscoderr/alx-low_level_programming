6450a4e9671ee5f69b982f62dbcd3c0dbc67df9c
youness <younesselayyoubi@hotmail.com>
2023-03-27 08:18:54 -0700

    prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * Description: prints a string in reverse
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int  i = strlen(s) - 1;
+
+	while (s[i] >= 0)
+	{
+		_putchar(s[i]);
+
+		i--;
+	}
+	_putchar('\n');
+
+}
