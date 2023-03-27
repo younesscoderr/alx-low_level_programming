bb4f8b5af553c13f55c0c315dedbe522455d04d6
youness <younesselayyoubi@hotmail.com>
2023-03-27 04:19:05 -0700

    reset the value of pointer *n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Dscription: updates the value that *n points to to 98
+ * @n: pointer to update the value it points to
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_
+#define MAIN_H
+
+#include <stdio.h>
+void reset_to_98(int *n);
+
+#endif
