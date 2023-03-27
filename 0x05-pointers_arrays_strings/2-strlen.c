74922f4d2f63952dc2c57812eb2a6ac5a946c2ce
youness <younesselayyoubi@hotmail.com>
2023-03-27 05:15:54 -0700

    count the length of a given string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry poin
+ * Description: returns the length of a string
+ * @s: string to return its length
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
