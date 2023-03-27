4972e7672d099e8fa40214e9666ca366a88ac9d9
youness <younesselayyoubi@hotmail.com>
2023-03-27 09:06:02 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,13 @@
 void print_rev(char *s)
 {
 	int  i;
-	int len = _strlen(s);
+	int len = 0;
 
-	i = len - 1;
-	while (s[i] >= 0)
-	{
+	len = _strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 
-		i--;
-	}
 	_putchar('\n');
 
 }
