9373fca393bdab93e9c3f434a10b85a56fb83fd6
youness <younesselayyoubi@hotmail.com>
2023-04-08 04:00:04 -0700

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ *
+ * Description: prints the name of the program followed by new line.
+ * @argc: argument count
+ * @argv: argument vector.
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
