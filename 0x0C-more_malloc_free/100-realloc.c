5f7b161d59774901903e59539acad0de544e4d10
youness <younesselayyoubi@hotmail.com>
2023-04-13 10:01:45 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,6 +3,8 @@
 /**
  * _realloc - reallocates a memory block using malloc and free.
  *
+ * @ptr:  is a pointer to the memory previously allocated with
+ * a call to malloc: malloc(old_size)
  * @old_size: is  the size, in bytes, of the allocated space for ptr.
  * @new_size: is the new size, in bytes of the new memory block.
  *
