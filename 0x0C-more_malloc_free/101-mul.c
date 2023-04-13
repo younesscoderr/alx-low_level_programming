d5b690f3018396c2536cc7fc09d67327e50f1045
youness <younesselayyoubi@hotmail.com>
2023-04-13 14:00:25 -0700

    multiplies two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,23 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 #include <ctype.h>
-#include <unistd.h>
 
 int _strlen(char *s);
 int _isnumber(char *s);
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * mul - multiplies two positive numbers.
