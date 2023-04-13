7b95b998eda49f88b7fd5338ef4221545c6dd69f
youness <younesselayyoubi@hotmail.com>
2023-04-13 05:21:24 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = len2;
 	}
 
-	s3 = malloc((len1 + n + 1) * sizeof(char));
+	s3 = malloc((len1 + n) * sizeof(char));
 
 	if (s3 == NULL)
 	{
