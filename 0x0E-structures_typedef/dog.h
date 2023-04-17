d6f58314cbb0f697e3f56233cede5d952a81cc8a
youness <younesselayyoubi@hotmail.com>
2023-04-17 05:55:24 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a structure that defines a dog
+ * @name: the name of the dog
+ * @age: the age of the dog in years
+ * @owner: the name of the dog's owner
+ *
+ * Description: This structure is used to store information about a dog
+ */
+
 struct dog
 {
 	char *name;
