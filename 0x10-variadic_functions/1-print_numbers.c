22727c1122c3eb604ccb6947c9143418114b13d8
youness <younesselayyoubi@hotmail.com>
2023-04-20 10:04:29 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,17 +17,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-	{
-		separator = " ";
-	}
-
 	for (i = 0; i < n; i++)
 	{
 		p = va_arg(args, int);
 
 		printf("%d", p);
-		printf("%s", separator);
+
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
 	}
 	putchar('\n');
 }
