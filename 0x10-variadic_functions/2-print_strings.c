1880d2bb2df3a9fb7ca825a2a3b444a22e678abd
youness <younesselayyoubi@hotmail.com>
2023-04-20 11:12:38 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		s = va_arg(args, char*);
 
-		if (s == NULL || s[0] == '\0')
+		if (s == NULL)
 		{
 			printf("(nil)");
 		}
